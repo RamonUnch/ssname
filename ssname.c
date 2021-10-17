087917ec3d7814d8f3cbfe4384a5589b7809ccd3
RamonUnch <74856804+RamonUnch@users.noreply.github.com>
2021-10-17 22:01:28 +0200

    Update to 1.1
    Ton of fixes and improvements

--- ssname.c
+++ ssname.c
@@ -1,6 +1,6 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * SSNAME: Set short name automatically under windows NT (8.3 DOS name)  *
- * Tested under Windows XP and Windows 7.                                *
+ * VERSION 1.1 Tested under Windows XP and Windows 7.                    *
  * Copyright 2021 Raymond GILLIBERT                                      *
  * I wrote this program to re-generate shortnames automatically after    *
  * copying some files under Linux using NTFS-3G.                         *
@@ -12,6 +12,8 @@
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - *
  * Note that it will start at ~0 and go up to ~ZZ (Base 36)              *
  * This is bad code I wrote in one night with some good drink.....       *
+ * Since 1.1 you no longer need to prefix \\?\ to a path in order to     *
+ * use long-path names.                                                  *
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - *
  * This file is under the DWTFPL.                                        *
  * DO WHAT THE FUCK YOU WANT WITH THIS SOFTWARE                          *
@@ -19,17 +21,41 @@
 
 #include <windows.h>
 #include <stdio.h>
+
 /*
-build command line I used (yes I kno I am a dummy trying to look cool):
+build command line I used (yes I know I am a dummy trying to look cool):
 
 gcc -m32 -march=i386 ssname.c -o ssname.exe -nostdlib -e_unfuckMain@0 -s -Wl,-s,-dynamicbase,-nxcompat,--no-seh,--relax,--disable-runtime-pseudo-reloc,--enable-auto-import,--disable-stdcall-fixup -fno-stack-check -fno-stack-protector -fno-ident -fomit-frame-pointer -mno-stack-arg-probe -m32 -march=i386 -mtune=i686 -mpreferred-stack-boundary=2 -momit-leaf-frame-pointer -fno-exceptions -fno-dwarf2-cfi-asm -fno-asynchronous-unwind-tables -lkernel32 -lmsvcrt -luser32 -ladvapi32 -D__USE_MINGW_ANSI_STDIO=0 -Wall
 
 */
 
 ///////////////////////////////////////////////////////////////////////////
+// Beter print stuff to display unicode in the prompt...
+HANDLE hCONSOLE;
+static BOOL printW(const wchar_t *s)
+{
+    return WriteConsoleW(hCONSOLE, s, wcslen(s), NULL, NULL);
+}
+static void LastErrorEndl(DWORD err)
+{
+    wchar_t txt[16];
+    wchar_t str[32];
+    wcscpy(str, L", Error=");
+    wcscat(str, _ultow(err, txt, 10));
+    wcscat(str, L"\n");
+    printW(str);
+}
+static void printWErr(const wchar_t *buf1, const wchar_t *buf2)
+{
+    DWORD err = GetLastError();
+    printW(buf1);
+    if(buf2) printW(buf2);
+    LastErrorEndl(err);
+}
+///////////////////////////////////////////////////////////////////////////
 // Copy pasted from msdn...
 // We need this function to get the privilege to modify the short name
-HRESULT ModifyPrivilege(IN LPCTSTR szPrivilege, IN BOOL fEnable)
+static HRESULT ModifyPrivilege(IN LPCTSTR szPrivilege, IN BOOL fEnable)
 {
     HRESULT hr = S_OK;
     TOKEN_PRIVILEGES NewState;
@@ -38,14 +64,14 @@ HRESULT ModifyPrivilege(IN LPCTSTR szPrivilege, IN BOOL fEnable)
 
     // Open the process token for this process.
     if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES|TOKEN_QUERY, &hToken )) {
-        wprintf(L"Failed OpenProcessToken\n");
+        printWErr(L"Failed OpenProcessToken", NULL);
         return ERROR_FUNCTION_FAILED;
     }
 
     // Get the local unique ID for the privilege.
     if ( !LookupPrivilegeValue( NULL, szPrivilege, &luid )) {
         CloseHandle( hToken );
-        wprintf(L"Failed LookupPrivilegeValue\n");
+        printWErr(L"Failed LookupPrivilegeValue", NULL);
         return ERROR_FUNCTION_FAILED;
     }
 
@@ -56,7 +82,7 @@ HRESULT ModifyPrivilege(IN LPCTSTR szPrivilege, IN BOOL fEnable)
 
     // Adjust the token privilege.
     if (!AdjustTokenPrivileges(hToken, FALSE, &NewState, 0, NULL, NULL)) {
-        wprintf(L"Failed AdjustTokenPrivileges\n");
+        printWErr(L"Failed AdjustTokenPrivileges", NULL);
         hr = ERROR_FUNCTION_FAILED;
     }
 
@@ -69,7 +95,7 @@ HRESULT ModifyPrivilege(IN LPCTSTR szPrivilege, IN BOOL fEnable)
 // Conversion function UTF-16 -> CP_ACP
 // We transform all non ansi chars into '_'
 #define FLAGS WC_COMPOSITECHECK|WC_DISCARDNS|WC_SEPCHARS|WC_DEFAULTCHAR
-char *utf16_to_ACP(const wchar_t *input)
+static char *utf16_to_ACP(const wchar_t *input)
 {
     char *acp=NULL;
     size_t BuffSize = 0, Result = 0;
@@ -90,36 +116,31 @@ char *utf16_to_ACP(const wchar_t *input)
 
     return NULL;
 }
-///////////////////////////////////////////////////////////////////////////
-// Removes the trailing file name from a path
-static BOOL PathRemoveFileSpecL(wchar_t *p)
+static wchar_t *GetFNinPath(wchar_t *p)
 {
     int i=0;
-    if (!p) return FALSE;
-
-    while(p[++i] != '\0');
-    while(i > 0 && p[i] != '\\' && p[i] != '/') i--;
-    i++;
-    i +=  ((p[i+1] == '\\') | (p[i+1] == '/'));
-    p[i]='\0';
-
-    return TRUE;
-}
-
-///////////////////////////////////////////////////////////////////////////
-// Removes the path and keeps only the file name
-static void PathStripPathL(wchar_t *p)
-{
-    int i=0, j;
-    if (!p) return;
 
     while(p[++i] != '\0');
     while(i >= 0 && p[i] != '\\' && p[i] != '/') i--;
     i++;
-    for(j=0; p[i+j] != '\0'; j++) p[j]=p[i+j];
-    p[j]= '\0';
+    i += (p[i] == '\\' || p[i] == '/');
+    return &p[i]; // first char of the filename
+
 }
-static int IsBadChar(wchar_t c)
+///////////////////////////////////////////////////////////////////////////
+// Removes the trailing file name from a path
+// and returns the pointer to filename only
+static wchar_t *SeparatePathAndFN(wchar_t *p)
+{
+    wchar_t *fn = GetFNinPath(p);
+
+    if(fn > p) fn[-1] = '\0'; // Only if we are within the p range...
+
+    return fn;
+}
+///////////////////////////////////////////////////////////////////////////
+// Removes the path and keeps only the file name
+static BOOL IsBadChar(const wchar_t c)
 {
     return c < 33 || c > 127 // Control and non ASCHII
        || (c > 57 && c < 64) // : ; < = > ?
@@ -128,7 +149,7 @@ static int IsBadChar(wchar_t c)
 }
 ///////////////////////////////////////////////////////////////////////////
 // Tell is there are any non-shortname compatible char in fn
-static int IsCoolStr(wchar_t *fn)
+static BOOL IsCoolStr(const wchar_t *fn)
 {
     int i, npoints=0;
     for (i=0; fn[i]; i++) {
@@ -139,7 +160,7 @@ static int IsCoolStr(wchar_t *fn)
     return npoints <= 1; // Cool
 }
 ///////////////////////////////////////////////////////////////////////////
-void StripUnCool(char* s)
+static void StripUnCool(char* s)
 {
     char *d = s;
     do {
@@ -160,45 +181,36 @@ static void StripPoints(char *s)
     } while ((*s++ = *d++));
 }
 ///////////////////////////////////////////////////////////////////////////
-static int IsShortName(wchar_t *fn_)
+// Determines if a filename really has a vaid short-name.
+static wchar_t *IsShortName(wchar_t *fn_)
 {
-    int ret=0;
-    wchar_t *fn = calloc(wcslen(fn_)+16, sizeof(wchar_t));
-    wcscpy(fn, fn_);
-    PathStripPathL(fn); // must already be stripped...
+    wchar_t *fn = GetFNinPath(fn_);
     int len;
-    if ((len = wcslen(fn)) < 13  && IsCoolStr(fn)) {
-        wchar_t *point;
-        if ((point = wcschr(fn, L'.'))) {
-            if((fn + len-1)-point <= 3) {
-                ret = 1;
-            }
-        } else {
-            // No extension in file.
-            // then max legth is 8!
-            ret = (len < 9);
-        }
+    wchar_t *point;
+    if (((len = wcslen(fn)) < 13  )
+    && (IsCoolStr(fn))
+    // The POINT has to be maximum 3 char from the end if there is one.
+    && (((point = wcschr(fn, L'.')) && (fn + len-1)-point <= 3)
+      ||(len <= 8)) ) { // No extension in file, then max legth is 8!
+        return fn;
     }
-    free(fn);
-    return ret;
+    return NULL;
 }
 
+#define FileExistsW(fn) (INVALID_FILE_ATTRIBUTES != GetFileAttributesW(fn))
 ///////////////////////////////////////////////////////////////////////////
-static int FileExistsW(const wchar_t *fn, const wchar_t *path)
+// Version that takes separately the path and the filename
+// note that path has to be long enough so that path+fn+1 can fit inside...
+static int FileExistsInPath(const wchar_t *fn, wchar_t *path)
 {
-    if(!fn || !fn[0]) return -1;
-    if(!path || !path[0]) return INVALID_FILE_ATTRIBUTES != GetFileAttributesW(fn);
-
-    wchar_t *tmpstr = calloc(wcslen(fn)+wcslen(path)+16, sizeof(wchar_t));
-    if (!tmpstr) return -1;
-    wcscpy(tmpstr, path);
-    wcscat(tmpstr, fn);
-    //_putws(tmpstr);
-    int ret = INVALID_FILE_ATTRIBUTES != GetFileAttributesW(tmpstr);
-    free(tmpstr);
+    size_t pathlen = wcslen(path);
+    path[pathlen] ='\\'; // Add the backslash.
+    path[pathlen+1] ='\0'; // and Null terminate
+    wcscat(path, fn);
+    int ret = INVALID_FILE_ATTRIBUTES != GetFileAttributesW(path);
+    path[pathlen] = '\0'; // put back the NULL terminator.
     return ret;
 }
-
 
 // Note that we could also use '!', '&', '#', '@' '$' ''' '^' '{' '}' '`'
 static const char g_mapping[] =  {
@@ -210,9 +222,8 @@ static const char g_mapping[] =  {
 #define BASE (sizeof(g_mapping)/sizeof(g_mapping[0]))
 
 ///////////////////////////////////////////////////////////////////////////
-static void strtowide(wchar_t *shorty, char *sfn)
+static void strntowide(wchar_t *shorty, const char *sfn, size_t len)
 {
-    int len = strlen(sfn)+1;
     int i;
     for (i=0; i < len; i++) {
         shorty[i] = sfn[i];
@@ -225,10 +236,9 @@ static void strtowide(wchar_t *shorty, char *sfn)
 // Finallly truncate the name appending a ~X sufix, and check that file
 // does not already exists.
 // This is super slow, stupid and bad coding but whatever...
-static int NewShortname(wchar_t *shorty, wchar_t *fnW, wchar_t *path)
+static int NewShortname(wchar_t *sfn, wchar_t *fnW, wchar_t *path)
 {
-    PathStripPathL(fnW);
-    _wcsupr(fnW);
+    _wcsupr(fnW); // UPPERCASE!!!
     char *fn = utf16_to_ACP(fnW);
     StripUnCool(fn);
 
@@ -237,28 +247,26 @@ static int NewShortname(wchar_t *shorty, wchar_t *fnW, wchar_t *path)
     while(i >= 0 && fn[i] != '.') i--;
     fn[i] = '\0'; // separate ext and fn.
     int iext = ++i; //File extension
-    char sext[5] = {'.', 0, 0, 0, 0};
+    wchar_t sext[5] = {'.', 0, 0, 0, 0};
     if (iext) {
-        strncpy(&sext[1], &fn[iext], 3);
-        //puts(sext);
+        strntowide(&sext[1], &fn[iext], 3);
+        //printWErr(L"File extension ", sext);
     }
     // if there is a file extension then we stop to the point
     // We always cap to 6 for the ~X after...
-    char sfn[13];
     StripPoints(fn);
-    ZeroMemory(sfn, sizeof(sfn));
-    strncpy(sfn, fn, iext? min(6, iext-1): 6);
+
+    strntowide(sfn, fn, iext? min(6, iext-1): 6);
     free(fn);
-    int slen = strlen(sfn); // max is 6.
+    int slen = wcslen(sfn); // max is 6.
+
     sfn[slen] = '~'; // TILDE
     // We only tru 0-9 and A-Z ie 36 tests max!
-
     for (i=0; i < BASE; i++) {
         sfn[slen+1] = g_mapping [i];
         sfn[slen+2] = '\0';
-        if (iext) strcat(sfn, sext);
-        strtowide(shorty, sfn);
-        if (!FileExistsW(shorty, path)) {
+        if (iext) wcscat(sfn, sext);
+        if (!FileExistsInPath(sfn, path)) {
             // We are done!
             return i+1;
         }
@@ -274,9 +282,8 @@ static int NewShortname(wchar_t *shorty, wchar_t *fnW, wchar_t *path)
         sfn[slen+2] = g_mapping[i%BASE];
         sfn[slen+3] = '\0';
 
-        if (iext) strcat(sfn, sext);
-        strtowide(shorty, sfn);
-        if (!FileExistsW(shorty, path)) {
+        if (iext) wcscat(sfn, sext);
+        if (!FileExistsInPath(sfn, path)) {
             // We are done!
             return i+37;
         }
@@ -284,27 +291,78 @@ static int NewShortname(wchar_t *shorty, wchar_t *fnW, wchar_t *path)
     // Max possible collisions is 36*36+36 = 1332;
     return 0;
 }
+static wchar_t *GetUNCPath(const wchar_t *FileName)
+{
+    // Actually for GetFullFileName it is not needed to
+    // add the funny \\?\ prefix to get the full path name.
+    // However we add it in to the buffer for future uses...
+    // also we should not add it if it is alrady an UNC
+    // and we should prefix with \\?\UNC\ in case of Network path
+    ULONG len = GetFullPathNameW(FileName, 0, 0, 0);
+    if (len) {
+        wchar_t *buf = calloc(len + 16, sizeof(wchar_t));
+        if (!buf) return NULL;
+        int buffstart;
+        if (FileName[0] == '\\' && FileName[1] == '\\' && FileName[2] == '?') {
+            // Already an UNC...
+            buffstart = 0;
+        } else if (FileName[0] == '\\' && FileName[1] == '\\') {
+            // Network path "\\server\share" style...
+            buf[0] = '\\'; buf[1] = '\\';  buf[2] = '?'; buf[3] = '\\';
+            buf[4] = 'U'; buf[5] = 'N';
+            buffstart = 6;
+        } else if (FileName[2] == '\\' && FileName[3] == '\\') {
+            // Network path "C:\\server\share" style...
+            buf[0] = '\\'; buf[1] = '\\';  buf[2] = '?'; buf[3] = '\\';
+            buf[4] = 'U'; buf[5] = 'N';
+            buffstart = 6;
+            FileName = &FileName[2]; // convert to \\server\share style
+        } else {
+            // Relative or non UNC path.
+            buf[0] = '\\'; buf[1] = '\\';  buf[2] = '?'; buf[3] = '\\';
+            buffstart = 4;
+        }
+        // Get the real pathname this time...
+        if (GetFullPathNameW(FileName, len, &buf[buffstart], NULL)) {
+            // if Network path
+            if(buffstart == 6) {
+                buf[6] = 'C'; // "\\?\UNC\server\share" style
+            }
+//            printWErr(L"FullPathName=", buf);
+            return buf;
+        } else {
+            printWErr(L"Unable to get full path name for ", FileName);
+            free (buf);
+        }
+    }
+    return NULL;
+}
 ///////////////////////////////////////////////////////////////////////////
 // Main with UNICODE support!
 int wmain(int argc, wchar_t **argv)
 {
     int ret = 0;
     if (argc !=2) {
-        wprintf(L"usage: ssname.exe filename\n");
+        printW(L"Usage: ssname.exe filename\n");
         return 1;
     }
-    wchar_t *fn = argv[1];
 
-    if (!FileExistsW(fn, NULL)) {
-        wprintf(L"File %s does not exist!\n", fn);
+    // fn will be the full path with the \\?\ prefix
+    // So LONG-PATH will be handled.
+    wchar_t *fn = GetUNCPath(argv[1]);
+    if(!fn) {
+        return 1;
+    } else if (!FileExistsW(fn)) {
+        printWErr(fn, L" Not found!");
+        free(fn);
         return 1;
     }
     DWORD slen = GetShortPathNameW(fn, NULL, 0);
     wchar_t *sfn = calloc(slen+16, sizeof(wchar_t));
+    wchar_t *shortname;
     if (slen) slen = GetShortPathNameW(fn, sfn, slen);
-    if (slen && IsShortName(sfn)) {
-        PathStripPathL(sfn);
-        wprintf(L"Already short-named: %s\n", sfn);
+    if (slen && (shortname = IsShortName(sfn))) {
+        printWErr(L"Already short-named: ", shortname);
     } else {
         // No existing short name!
         ModifyPrivilege(SE_RESTORE_NAME, TRUE);
@@ -314,32 +372,31 @@ int wmain(int argc, wchar_t **argv)
                                  , OPEN_EXISTING
                                  , FILE_FLAG_BACKUP_SEMANTICS, NULL);
         if(!hfile) {
-            wprintf(L"error: file %s cannot be opened, error=%lu\n", fn, GetLastError());
+            printWErr(fn, L" cannot be opened");
+            free(fn);
             return 1;
         }
         // Separate path from file...
-        wchar_t *path = calloc(wcslen(fn)+16, sizeof(wchar_t));
-        wcscpy(path, fn);
-        PathRemoveFileSpecL(path);
-        PathStripPathL(fn);
+        wchar_t *path = fn;
+        wchar_t *filename = SeparatePathAndFN(path);
 
         // We need to create a new shortname names shorty!
         // This one should not include the path...
         wchar_t shorty[14];
         ZeroMemory(shorty, sizeof(shorty));
-        if (!NewShortname(shorty, fn, path)) {
-            wprintf(L"Unable to find a new valid shortname for %s\n", fn);
+        if (!NewShortname(shorty, filename, path)) {
+            printWErr(filename, L" Unable to find a new valid shortname!\n");
             ret = 1;
         }
         if (!ret && SetFileShortNameW(hfile, shorty)) {
-            wprintf(L"SHORT-NAME SET TO: %s\n", shorty);
+            printWErr(L"SHORT-NAME SET TO: ", shorty);
         } else {
-            wprintf(L"Unable to set short-name: %s, error=%lu\n", shorty, GetLastError());
+            printWErr(L"Unable to set short-name: ", shorty);
         }
         CloseHandle(hfile);
         ModifyPrivilege(SE_RESTORE_NAME, FALSE);
-        free(path);
     }
+    free(fn);
     return ret;
 }
 
@@ -350,5 +407,6 @@ int WINAPI unfuckMain(void)
     long argc, l;
     wchar_t **argv, **Env_;
     __wgetmainargs(&argc, &argv, &Env_,1 , &l);
+    hCONSOLE = GetStdHandle(STD_OUTPUT_HANDLE);
     ExitProcess(wmain(argc, argv));
 }
